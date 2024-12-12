d2affe312d3bd163ec2f1877141414f57ae44da5
JulioFreitas <juliosparapan2014@gmail.com>
2024-12-12 11:50:36 -0300

    Alterações para login frontend

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -7,7 +7,7 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  apiUrl: string = "http://localhost:8080/auth"
+  apiUrl: string = "http://50.6.198.227:8080/auth"
 
   constructor(private httpClient: HttpClient) { }
 
